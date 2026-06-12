e38065562b6474b9638e3b83edf94c4862824984
Clayton Bollinger <clayherb99@gmail.com>
2026-06-12 08:28:17 -0400

    Initial Nuxt

---
+++ app/app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Welcome to Blackjack!</h1>
+    <Game />
+  </div>
+</template>
---
+++ app/components/game.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const cards = ["Old Card"]
+
+const drawCard = () => {
+  cards.push("New Card")
+}
+</script>
+
+<template>
+  <div> You have drawn the following cards: </div>
+  <div v-for="card in cards" :key="card">
+    {{ card}}
+  </div>
+  <button @click="drawCard()">Draw</button>
+</template>
+
+<style scoped>
+
+</style>
