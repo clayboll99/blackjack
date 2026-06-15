51aeeadc981fd8fc7843b46eb918fcafca1962c3
Clayton Bollinger <47898520+clayboll99@users.noreply.github.com>
2026-06-15 15:26:27 +0000

    Add pretttier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
 // @ts-check
+import eslintConfigPrettier from 'eslint-config-prettier'
 import withNuxt from './.nuxt/eslint.config.mjs'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default withNuxt(
   // Your custom configs here
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended
 )
