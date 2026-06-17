ab03e07278e36f6cd903eb780aa8b8b1e4f1a2c4
Clayton Bollinger <47898520+clayboll99@users.noreply.github.com>
2026-06-17 16:58:00 +0000

    Increase space between game actions buttons

--- app/components/GameActions.vue
+++ app/components/GameActions.vue
@@ -5,7 +5,7 @@ const game = useGameStore()
 <template>
   <UContainer v-if="game.winner === ''">
     <UButton @click="game.drawCard()">Hit</UButton>
-    <UButton class="ml-px" @click="game.dealersTurn()">Stand</UButton>
+    <UButton class="ml-[5px]" @click="game.dealersTurn()">Stand</UButton>
   </UContainer>
   <UContainer v-else>
     The winner is {{ game.winner }}!
