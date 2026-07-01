b610383b96461663a3f2e8adde603b02c46e8e4e
Clayton Bollinger <clayherb99@gmail.com>
2026-07-01 12:46:53 -0400

    Justify Content

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,8 +4,8 @@ const loginUrl = import.meta.env.PROD ? '/auth/github' : '/auth/keycloak'
 </script>
 
 <template>
-  <UButton v-if="!loggedIn" class="object-center" @click="openInPopup(loginUrl)"
-    >Login</UButton
-  >
-  <UButton v-else to="/game" class="object-center">Start a Game</UButton>
+  <div class="flex justify-center">
+    <UButton v-if="!loggedIn" @click="openInPopup(loginUrl)">Login</UButton>
+    <UButton v-else to="/game">Start a Game</UButton>
+  </div>
 </template>
